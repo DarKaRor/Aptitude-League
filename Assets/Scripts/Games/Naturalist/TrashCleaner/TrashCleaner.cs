5b015345c25ae5ef8587c1beac23d1940b191541
DarKaRor <jalp_18@hotmail.com>
2022-05-31 20:36:15 -0400

    Test

--- Assets/Scripts/Games/Naturalist/TrashCleaner/TrashCleaner.cs
+++ Assets/Scripts/Games/Naturalist/TrashCleaner/TrashCleaner.cs
@@ -37,7 +37,8 @@ public class TrashCleaner : MonoBehaviour
     
     void Start()
     {
-        GameManager.sharedInstance.currentGame = gameId;
+       // I hate programming
+       GameManager.sharedInstance.currentGame = gameId;
        collectableSprites = new()
        {
            { Collectables.Trash, trash },
